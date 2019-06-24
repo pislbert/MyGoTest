705c1b759a4a34226168d3e4e4aaec6393d54b6c
sven <q9090960bnb3@163.com>
2019-06-24 22:45:42 +0800

    go mod 入门

---
+++ go.mod
@@ -0,0 +1,5 @@
+module MyGoTest
+
+go 1.12
+
+require github.com/pislbert/MyGoUtil v0.0.0-20190307040828-c598e6772bf9
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import(
+	"github.com/pislbert/MyGoUtil/pt"
+)
+
+func main(){
+	pt.Ptn("hello pislbert and sven!")
+}
